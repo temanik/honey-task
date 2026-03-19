f4cebb6e3386c8f8841317aed3cafa66d9bfde1b
Николаев Артём <35996770+temanik@users.noreply.github.com>
2026-03-19 12:52:17 +0300

    solution

--- concurrency/task034/main.go
+++ concurrency/task034/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"runtime"
 	"sync"
 	"time"
 )
@@ -23,7 +24,9 @@ type RequestDeduplicator struct {
 func New() *RequestDeduplicator {
 	// YOUR CODE HERE
 	// Инициализируйте карту calls и верните указатель на RequestDeduplicator
-	return nil
+	return &RequestDeduplicator{
+		calls: make(map[string]*call),
+	}
 }
 
 // Do гарантирует, что для данного ключа функция fn будет выполнена только один раз
@@ -31,9 +34,30 @@ func New() *RequestDeduplicator {
 // завершения первого и получат тот же результат.
 // После завершения fn запись о ключе удаляется, чтобы будущие вызовы снова вызывали fn.
 func (rd *RequestDeduplicator) Do(key string, fn func() (interface{}, error)) (interface{}, error) {
-	// YOUR CODE HERE
-	// Реализуйте логику дедупликации, используя rd.mu, rd.calls и call.wg
-	return nil, nil
+	rd.mu.Lock()
+	c, ok := rd.calls[key]
+	if ok {
+		rd.mu.Unlock()
+		c.wg.Wait()
+		return c.val, c.err
+	}
+
+	c = &call{}
+	c.wg.Add(1)
+	rd.calls[key] = c
+	rd.mu.Unlock()
+
+	c.val, c.err = fn()
+	c.wg.Done()
+
+	go func() {
+		time.Sleep(3 * time.Second)
+		rd.mu.Lock()
+		delete(rd.calls, key)
+		rd.mu.Unlock()
+	}()
+
+	return c.val, c.err
 }
 
 // Пример использования (не требует изменений)
@@ -49,19 +73,29 @@ func main() {
 			result, err := rd.Do(keys[i%2], func() (interface{}, error) {
 				fmt.Printf("  [%d] Выполняется реальный вызов API...\n", id)
 				time.Sleep(2 * time.Second)
-				return "Weather: sunny", nil
+				runtime.Gosched()
+				return "Weather: sunny", fmt.Errorf("api error")
 			})
 			fmt.Printf("  [%d] Результат %d: %v, err: %v\n", id, i%2, result, err)
 		}(i)
 	}
 	wg.Wait()
 
-	// Проверка, что следующий вызов запустит функцию снова (кеш не хранится)
+	// Проверка, что следующий вызов запустит функцию снова (кеш хранится)
 	time.Sleep(1 * time.Second)
 	fmt.Println("\nНовый вызов через 1 секунду:")
 	result, err := rd.Do(keys[0], func() (interface{}, error) {
 		fmt.Println("  Реальный вызов API (новый)")
 		return "Weather: cloudy", nil
 	})
-	fmt.Printf("Результат: %v, err: %v\n", result, err)
+	fmt.Printf("Cached Результат: %v, err: %v\n", result, err)
+
+	// Проверка, что следующий вызов запустит функцию снова (кеш удалился)
+	time.Sleep(3 * time.Second)
+	fmt.Println("\nНовый вызов через 4 секунду:")
+	result, err = rd.Do(keys[0], func() (interface{}, error) {
+		fmt.Println("  Реальный вызов API (новый)")
+		return "Weather: cloudy", nil
+	})
+	fmt.Printf("New Результат: %v, err: %v\n", result, err)
 }
